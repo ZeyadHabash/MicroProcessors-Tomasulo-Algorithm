ba9385e4ddece3ef63761dacd8be4e42a13ec1a6
Zeyadptd <zeyad.habashy02@gmail.com>
2023-12-10 12:51:17 +0200

    Minor edits to GUI

--- src/TextAreaOutputStream.java
+++ src/TextAreaOutputStream.java
@@ -3,13 +3,14 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 import javax.swing.*;
+import javax.swing.text.BadLocationException;
 
 public class TextAreaOutputStream extends OutputStream {
     private JTextArea textArea;
 
     public TextAreaOutputStream(JTextArea textArea) {
         this.textArea = textArea;
-        textArea.setFont(new Font("Arial", Font.PLAIN, 14));
+        textArea.setFont(new Font("Arial", Font.PLAIN, 16));
     }
 
     @Override
@@ -22,15 +23,23 @@ public class TextAreaOutputStream extends OutputStream {
         SwingUtilities.invokeLater(() -> textArea.append(str));
     }
 
+    private int lastIndex = 0; // Add this field to keep track of the last index
+
     public void search(String searchString) {
         String textAreaContent = textArea.getText();
-        int index = textAreaContent.indexOf(searchString);
+        int index = textAreaContent.indexOf(searchString, lastIndex);
         if (index >= 0) { // String found
             textArea.setCaretPosition(index);
             textArea.moveCaretPosition(index + searchString.length());
             textArea.getCaret().setSelectionVisible(true);
+            lastIndex = index + searchString.length();
         } else {
-            JOptionPane.showMessageDialog(null, "Search string not found", "Info", JOptionPane.INFORMATION_MESSAGE);
+            if (lastIndex > 0) { // If end of text is reached, start from the beginning
+                lastIndex = 0;
+                search(searchString);
+            } else {
+                JOptionPane.showMessageDialog(null, "Search string not found", "Info", JOptionPane.INFORMATION_MESSAGE);
+            }
         }
     }
 }
